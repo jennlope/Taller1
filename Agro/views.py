4ffb48a50e5cd8f8579fce09c12689c0dd8634b5
Jennifer <lopezgomezjennifer50@hotmail.com>
2024-08-01 15:01:46 -0500

    Actualiza views.py y home.html para mejorar la búsqueda de productos

--- Agro/views.py
+++ Agro/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Agro
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to AgroMerc</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Jennifer'})
+    searchTerm = request.GET.get('searchProduct')
+    if searchTerm:
+        agros = Agro.objects.filter(title__icontains=searchTerm)
+    else:
+        agros = Agro.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm,'agros':agros})
 
 def about(request):
     #return HttpResponse('<h1>Welcome to About page</h1>')
